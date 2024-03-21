0f83ef132d2f58e44bbf095e74824f7ecf2ba623
Gabo01K <gabriel.trujillo@ilb.edu.mx>
2024-03-20 18:53:48 -0600

    Se agregaron nuevos cambios al archivo xd

--- pandas_alexis.py
+++ pandas_alexis.py
@@ -1,4 +1,9 @@
 import pandas as pd
 azul=pd.read_csv("conejito_malo.csv")
 print (azul)
-#to_csv para la siguiente clase
+#to_csv para la sigunte clase
+print(prueba de github)
+
+//hjslsjdh
+
+//nuevo cambio
